32a46ca2784af0ad00fb11fb106f27e3a2a142b3
Iagobarros211256 <iagobarrosgomes2112@gmail.com>
2025-09-14 17:57:16 -0300

    finished main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,8 +4,13 @@ import (
 	"log"
 	"os"
 
+	"github.com/Iagobarross211256/rockshop/internal/handlers"
 	"github.com/Iagobarros211256/rockshop/internal/store"
 )
+
+// this was done by chat gpt. read it carefully
+// this will be replaced on future comnmits beacase 
+// is a manual db and server handling
 
 func main() {
 	dataFile := os.Getenv("DB_FILE")
@@ -23,3 +28,24 @@ func main() {
 		log.Fatalf("faailed to init store: %v", err)
 	}
 }
+
+r := gin.Default()
+
+//handlers
+ph := handlers.NewProductHandler(st)
+oh := handlers.NewOrderHandler(st)
+hh := handlers.NewHHealthCheckHandler()
+
+api := r.Group("/api/v1")
+{
+	api.GET("/health", hh.Health)
+	api.GET("/products", ph.ListProducts)
+	api.GET("/products/:id", ph.GetProduct)
+	api.POST("/products", ph.CreateProduct)
+	api.PUT("/products/:id", ph.UpdateProduct)
+	api.DELETE("/products/:id", ph.DeleteProduct)
+	api.POST("/orders", oh.CreateOrder)
+	api.GET("/orders/:id", oh.GetOrder)
+	api.GET("/orders", oh.ListOrders)
+}
+log.Printf("RockShop starting on :%s (DB: %s)", port, dataFile)
